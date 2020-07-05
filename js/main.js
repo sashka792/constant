b723bee732e6acf6aea987310b6f34245ec04bb2
sashka792 <sashka792@gmail.com>
2020-07-05 16:29:05 +0300

    add function for contact form

--- js/main.js
+++ js/main.js
@@ -20,4 +20,21 @@ $(document).ready(function () {
 //         $('.position').removeClass('hover');
 //         $(this).parent('.position').addClass('hover');
 //     });
+
+    //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 });
